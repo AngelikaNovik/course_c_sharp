3f4f5eca611fbea0fc8d02876c8d14bdf2a842e9
AngelikaNovik <bagirrra82@gmail.com>
2022-07-11 20:39:38 +0500

    последняя цифра

---
+++ HomeWork/HW_1_2/1_4/Program.cs
@@ -0,0 +1,5 @@
+﻿// на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a%10);
