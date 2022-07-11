0689474d1b159afaa00ec16987f107cddfabe258
AngelikaNovik <bagirrra82@gmail.com>
2022-07-11 20:28:11 +0500

    четные от 1 до N

---
+++ HomeWork/HW_1_2/1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+int i = 2;
+if(i>2)
+Console.WriteLine(a-1);
+while (i<=a)
+Console.WriteLine(i);
+i=i+2;
+
