7836c569477ed5970a02a97b6c791830856711cd
AngelikaNovik <bagirrra82@gmail.com>
2022-07-11 20:02:40 +0500

    показать четное

---
+++ HomeWork/HW_1_2/1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//на вход принимает число и выдаёт, является ли число чётным
+
+Console.WriteLine("Write namber a: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0) ;
+
+Console.WriteLine("четное");
+Console.WriteLine("не четное");
+
+
