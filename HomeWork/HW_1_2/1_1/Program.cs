ca70b13457999703029daba8348134a3272c91cd
AngelikaNovik <bagirrra82@gmail.com>
2022-07-11 17:36:12 +0500

    выдает max из 3

---
+++ HomeWork/HW_1_2/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Write a nambers: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write b nambers: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Write c nambers: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if(max<b) max = b;
+if(max<c) max = c;
+
+Console.Write("max= ");
+Console.Write(max);
