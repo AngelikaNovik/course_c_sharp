58ee289a6786ed7ecab0dd794790adb94bba9fb0
AngelikaNovik <bagirrra82@gmail.com>
2022-07-11 14:01:59 +0500

    max/min

---
+++ HomeWork/HW_1/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// на вход 2 числа на выход какое больше и какое меньше
+
+Console.WriteLine("Write a namber; ");
+Console.WriteLine("Write b namber; ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int min = -9;
+int max = -3;
+
+Console.Write("max=");
+Console.WriteLine(max);
+
+Console.Write("min=");
+Console.WriteLine(min);
+
