b0ffd844ee824b93fdc0c41eb58c803b3387f3ea
AngelikaNovik <bagirrra82@gmail.com>
2022-07-14 19:43:03 +0500

    показывает вторую

---
+++ HomeWork/HW_2/1_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine((a/10)%10);
