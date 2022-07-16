6e5d9970b60cd478c380a745941f4b2b404033c7
AngelikaNovik <bagirrra82@gmail.com>
2022-07-16 12:42:04 +0500

    удаляет вторую

---
+++ HomeWork/HW_2/1_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a/100}{a%10}");
