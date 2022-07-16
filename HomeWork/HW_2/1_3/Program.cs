9294f6bb5cad7ed688599afc388f05fef86b929d
AngelikaNovik <bagirrra82@gmail.com>
2022-07-16 13:36:10 +0500

    третьей цифры нет

---
+++ HomeWork/HW_2/1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a > 99)
+{
+    Console.WriteLine(a%10);
+}
+else
+{
+    Console.WriteLine("-> третьей цифры нет");
+}
+
