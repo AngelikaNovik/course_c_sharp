356b34ad147991a40ae0a31c681fb6d6617a4c58
AngelikaNovik <bagirrra82@gmail.com>
2022-07-18 17:49:47 +0500

    является ли оно палиндромом

--- HomeWork/HW_3/1-1/Program.cs
+++ HomeWork/HW_3/1-1/Program.cs
@@ -1,1 +1,17 @@
 ﻿// на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"палиндром.");
+  }
+  else Console.WriteLine($"не палиндром.");
+}
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
