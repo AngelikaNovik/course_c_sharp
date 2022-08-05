118ec7ef18cf870d062d7b65693790e04cfee7e1
AngelikaNovik <bagirrra82@gmail.com>
2022-08-05 18:43:13 +0500

    выдаёт сумму цифр

---
+++ HomeWork/HW_4/1-2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("введите число ");
+int i = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (i > 0) ;
+{
+    int n = i % 10;
+    i = i / 10;
+    result = result + n;
+}
+Console.WriteLine("Сумма цифр: ");
